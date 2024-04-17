311e2bbddc2c3815f05678399ca431565346d5f7
Lyshka <lesha_novitskiy@mail.ru>
2024-04-17 16:08:03 +0300

    add mobile Main

--- src/script/Home/workSlider.ts
+++ src/script/Home/workSlider.ts
@@ -3,13 +3,22 @@ import { Navigation } from "swiper/modules";
 import "swiper/css";
 
 new Swiper("#slider-work", {
-  modules: [Navigation],
-  slidesPerView: 2,
-  spaceBetween: 20,
-  height: 519,
   loop: true,
+  modules: [Navigation],
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    1500: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      height: 519,
+    },
+    0: {
+      height: 200,
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
   },
 });
