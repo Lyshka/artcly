341c3bb2429c2265d9a60fd8bc8afc877729061b
Lyshka <lesha_novitskiy@mail.ru>
2024-05-10 12:55:02 +0300

    add new phone mask

--- src/script/General/phoneMask.ts
+++ src/script/General/phoneMask.ts
@@ -6,12 +6,11 @@ const phoneInput = [
 
 phoneInput.forEach((el) => {
   IMask(el, {
-    mask: "+375 (00) 000-00-00",
-    prepare: function (appended) {
-      if (/[0-9]/.test(appended)) {
-        return "+375" + appended;
-      }
-      return appended;
-    },
+    mask: [
+      {
+        mask: "+375 (00) 000-00-00",
+        lazy: false,
+      },
+    ],
   });
 });
